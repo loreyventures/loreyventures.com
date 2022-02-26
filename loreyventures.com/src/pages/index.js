d5f9d0faf9fcd10ffac161ec2df6a4886eb2d04a
Karl Lorey <git@karllorey.com>
2022-02-26 20:05:54 +0100

    Add basic content

--- loreyventures.com/src/pages/index.js
+++ loreyventures.com/src/pages/index.js
@@ -5,8 +5,12 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-      <main>
-          Hello World
+      <main className="container mx-auto p-10 text-center">
+          <h1 className="text-5xl">Lorey Ventures</h1>
+          <p className="text-gray-400 mt-5">
+              Lorey Ventures is the holding company of <a href="https://karllorey.com">Karl Lorey</a>.
+          </p>
+          <footer className="text-gray-400 mt-10">Built in Berlin with ❤</footer>
       </main>
   )
 }
